c3570f245bfc48afad6f530e13016837ce24b125
prospero <g0lden_vo1d@protonmail.com>
2023-10-07 17:01:26 +0200

    tweaks

--- config.c
+++ config.c
@@ -4,16 +4,11 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-mail",    600,  1 },
-    {"sb-music",   0,    2 },
-    {"sb-disk",    1800, 3 },
-    {"sb-memory",  10,   4 },
-    {"sb-loadavg", 5,    5 },
-    {"sb-mic",     0,    6 },
-    {"sb-record",  0,    7 },
-    {"sb-volume",  0,    8 },
-    {"sb-battery", 5,    9 },
-    {"sb-date",    1,    10},
+    {"bar-disk",    1800, 3 },
+    {"sb-memory",   10,   4 },
+    {"bar-volume",  0,    10},
+    {"bar-battery", 5,    9 },
+    {"bar-clock",   1,    10},
 };
 
 const unsigned short blockCount = LEN(blocks);
