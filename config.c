5026b0bba977bcc041d7e436ba43b80f7bc0d5ad
prospero <g0lden_vo1d@protonmail.com>
2023-10-08 16:52:40 +0200

    tweaks

--- config.c
+++ config.c
@@ -4,13 +4,12 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-disk",      1800, 3 },
-    {"sb-memory",    10,   4 },
-    {"sb-network",   5,    4 },
-    {"sb-volume",    0,    10},
-    {"sb-bluetooth", 5,    3 },
-    {"sb-battery",   120,    9 },
-    {"sb-clock",     1,    10},
+    {"sb-disk",    1800, 3 },
+    {"sb-memory",  10,   4 },
+    {"sb-network", 5,    4 },
+    {"sb-volume",  0,    10},
+    {"sb-bat",     120,  9 },
+    {"sb-clock",   1,    10},
 };
 
 const unsigned short blockCount = LEN(blocks);
