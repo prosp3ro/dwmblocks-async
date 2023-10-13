28d937057399210d294c9809ccb39b8d6782f8c0
prospero <g0lden_vo1d@protonmail.com>
2023-10-13 17:56:08 +0200

    sync

--- config.c
+++ config.c
@@ -7,7 +7,7 @@ Block blocks[] = {
     {"sb-disk",    1800, 3 },
     {"sb-memory",  10,   4 },
     {"sb-network", 5,    4 },
-    {"sb-volume",  0,    10},
+    {"sb-volume",  5,    10},
     {"sb-bat",     120,  9 },
     {"sb-clock",   1,    10},
 };
