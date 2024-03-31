ee04645fab97d763988ecaa018a23c68ed7663cc
prosp3ro <g0lden_vo1d@protonmail.com>
2024-03-31 13:16:43 +0200

    sync

--- config.c
+++ config.c
@@ -8,7 +8,7 @@ Block blocks[] = {
     { "sb-keyboard",   400,      0 },
     { "sb-disk",       300,      0 },
     { "sb-memory",     8,        0 },
-    { "sb-network",    6,        0 },
+    { "sb-network",    2,        0 },
     { "sb-volume",     4,        0 },
     { "sb-bat",        70,       0 },
     { "sb-clock",      60,       0 },
