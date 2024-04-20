16974d831e26b4a518ef96680e7b05d1ef091b59
prosp3ro <g0lden_vo1d@protonmail.com>
2024-04-20 11:03:12 +0200

    sync

--- config.c
+++ config.c
@@ -9,7 +9,7 @@ Block blocks[] = {
     { "sb-disk",       300,      0 },
     { "sb-memory",     8,        0 },
     { "sb-network",    2,        0 },
-    { "sb-publicip",   3600,     0 },
+    // { "sb-publicip",   3600,     0 },
     { "sb-vnstat",     20,       0 },
     { "sb-volume",     4,        0 },
     { "sb-bat",        70,       0 },
